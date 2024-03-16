e98e6ddc052a388763c6ad91c8e8d5d688513c24
luniemma <luniyisiemmanuel@gmail.com>
2024-03-16 16:39:38 -0400

    apply changes to names and little correction

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # infra-iac-terraform
+# terraform init
+# terraform vaildate
+# terraform plan
+# terraform apply --auto-approve
+# terraform destroy --auto-prove
--- main.tf
+++ main.tf
@@ -29,11 +29,11 @@ resource "azurerm_service_plan" "capplan" {
   resource_group_name = azurerm_resource_group.caprg.name
   location            = azurerm_resource_group.caprg.location
   sku_name            = "P1v2"
-  os_type             = "linux"
+  os_type             = "Linux"
 }
 
 resource "azurerm_windows_web_app" "capapp" {
-  name                = "capstone-web-app"
+  name                = "capstonewebapp"
   resource_group_name = azurerm_resource_group.caprg.name
   location            = azurerm_service_plan.capplan.location
   service_plan_id     = azurerm_service_plan.capplan.id
@@ -42,7 +42,7 @@ resource "azurerm_windows_web_app" "capapp" {
     always_on = false
     application_stack {
         current_stack = "dotnet"
-        dotnet_version = "v8.0"
+        dotnet_version = "v6.0"
     }
   }
   depends_on = [
@@ -50,19 +50,15 @@ resource "azurerm_windows_web_app" "capapp" {
    ]
 }
 
-resource "azurerm_mssql_server" "cap-sql" {
-  name                         = "capstone-sql-server"
+resource "azurerm_mssql_server" "capsql1624" {
+  name                         = "capstonesqlpro"
   resource_group_name          = azurerm_resource_group.caprg.name
   location                     = azurerm_resource_group.caprg.location
   version                      = "12.0"
-  administrator_login          = "missadministrator"
+  administrator_login          = "luniemma"
   administrator_login_password = "thisIsKat11"
   minimum_tls_version          = "1.2"
 
-  azuread_administrator {
-    login_username = "AzureAD Admin"
-    object_id      = "00000000-0000-0000-0000-000000000000"
-  }
 
   tags = {
     environment = "production"
@@ -70,11 +66,11 @@ resource "azurerm_mssql_server" "cap-sql" {
 }
 resource "azurerm_mssql_database" "cap-db" {
   name           = "capstone-db"
-  server_id      = azurerm_mssql_server.cap-sql.id
+  server_id      = azurerm_mssql_server.capsql1624.id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
   license_type   = "LicenseIncluded"
   max_size_gb    = 2
   read_scale     = true
-  sku_name       = "basic"
-  depends_on = [ azurerm_mssql_server.cap-sql ]
+  sku_name       = "P1"
+  depends_on = [ azurerm_mssql_server.capsql1624 ]
 }
